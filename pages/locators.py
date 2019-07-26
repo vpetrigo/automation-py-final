f97855840dda5781c611e156565e5c7e8c07516a
Vladimir Petrigo <taenaru@gmail.com>
2019-07-27 01:41:05 +0300

    automation: Clean up ALERT_ADDED_TO_CART selector

--- pages/locators.py
+++ pages/locators.py
@@ -21,7 +21,8 @@ class LoginPageLocators:
 class ProductPageLocators:
     BUTTON_ADD_TO_CART = (By.CLASS_NAME, "btn-add-to-basket")
     ADDING_SUCCESS = (By.CSS_SELECTOR, "div.alert-success")
-    ALERT_ADDED_TO_CART = (By.CSS_SELECTOR, "div.alertinner strong")
+    ALERT_ADDED_TO_CART = (By.CSS_SELECTOR,
+                           "#messages>div:first-child .alertinner strong")
     ALERT_CART_STATUS = (By.CSS_SELECTOR, ".alert-noicon.alert-info p")
     PRICE_VALUE = (By.CLASS_NAME, "price_color")
     PRODUCT_NAME = (By.TAG_NAME, "h1")
